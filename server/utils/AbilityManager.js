a5d653f0d4992c80d3544efc7fc6a6e62f0151bb
crugenstein <crugenstein@ufl.edu>
2025-03-04 14:31:56 -0500

    abilitymanager logic

--- server/utils/AbilityManager.js
+++ server/utils/AbilityManager.js
@@ -1,3 +1,5 @@
+const { GameManager } = require("./GameManager")
+
 class AbilityManager {
     static queue = []
 
@@ -6,7 +8,21 @@ class AbilityManager {
     }
     
     static processPhaseEnd() {
-        this.queue.sort((a, b) => a.ability.priority - b.ability.priority);
+        this.queue.sort((a, b) => a.ability.priority - b.ability.priority)
+        this.queue.forEach( ({user, ability, targetData}) => {
+            if (user) {
+                if (ability.selections == ["SELECT_SINGLE_PLAYER_TARGET"]) {
+                    GameManager.registerVisit(user, targetData[0])
+                }
+            }
+        })
+        this.queue.forEach( ({user, ability, targetData}) => {
+            if (ability.selections == ["SELECT_SINGLE_PLAYER_TARGET"]) {
+                ability.effect({user, target: targetData[0]})
+            }
+        })
+        this.queue = []
+        GameManager.clearVisits()
     }
 }
 
