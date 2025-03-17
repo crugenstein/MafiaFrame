7314074b880056cb8308aaaf134941cc1a07af1a
crugenstein <crugenstein@ufl.edu>
2025-03-17 15:15:27 -0400

    SharedChat polish

--- server/objects/SharedChat.js
+++ server/objects/SharedChat.js
@@ -1,3 +1,4 @@
+const { GameManager } = require('../utils/GameManager')
 const { IOManager } = require('../io/IOManager')
 const { v4: uuidv4 } = require('uuid')
 
@@ -7,81 +8,121 @@ const MessageType = Object.freeze({
     PLAYER_MESSAGE: 2
 })
 
-class SharedChat { // TODO OVERHAUL
-    constructor(name, readers, writers) {
-        this.name = name
-        this.chatId = uuidv4()
-        this.readers = new Set()
+class SharedChat {
+    /**
+    * Creates a new shared chat object. It receives a random UUID. Readers and writers are notified of gained access.
+    * Writers should be a subset of readers.
+    * @param {string} name - The name of the Shared Chat.
+    * @param {Array<string>} [readers] - List of player names to grant read access to.
+    * @param {Array<string>} [writers] - List of player names to grant write access to.
+    */
+    constructor(name, readers = [], writers = []) {
+        this._name = name
+        this._chatId = uuidv4()
+
         readers.forEach((readerName) => {this.addReader(readerName)})
-        this.writers = new Set()
         writers.forEach((writerName) => {this.addWriter(writerName)})
-        this.messages = []
+
+        this._messages = []
     }
 
+    /**
+    * Grants a player read access, sending the client a notification with chat data (including message history).
+    * @param {string} playerName - The name of the player to give read access to.
+    */
     addReader(playerName) {
         const player = GameManager.getPlayer(playerName)
 
-        player.chatsCanRead.add(this.chatId)
-        this.readers.add(playerName)
-
+        player.readableChats.add(this.chatId)
         IOManager.addPlayerToRoom(playerName, this.chatId)
-        IOManager.emitToPlayer(playerName, 'NEW_CHAT_READ_ACCESS', {chatId: this.chatId})
+        IOManager.emitToPlayer(playerName, 'NEW_CHAT_READ_ACCESS', {chatId: this.chatId, name: this.name, messages: this.messages})
     }
 
-    addWriter(playerName) {
+    /**
+    * Grants a player read/write access, sending the client a notification with chat id.
+    * @param {string} playerName - The name of the player to give read/write access to.
+    */
+    addRW(playerName) {
+        this.addReader(playerName)
         const player = GameManager.getPlayer(playerName)
-
-        player.chatsCanWrite.add(this.chatId)
-        this.writers.add(playerName)
-
+        
+        player.writeableChats.add(this.chatId)
         IOManager.emitToPlayer(playerName, 'NEW_CHAT_WRITE_ACCESS', {chatId: this.chatId})
     }
 
-    addRW(playerName) {
-        this.addReader(playerName)
-        this.addWriter(playerName)
-    }
-
+    /**
+    * Whether or not a player can read this shared chat.
+    * @param {string} playerName - The player name to check.
+    * @returns {boolean} Whether or not the player has read access.
+    */
     canRead(playerName) {
-        return this.readers.has(playerName)
-    }
-
-    canWrite(playerName) {
-        return this.writers.has(playerName)
-    }
-
-    revokeRead(playerName) {
         const player = GameManager.getPlayer(playerName)
-        player.removeReadableChat(this.chatId)
-        this.readers.delete(playerName)
+        if (!player) return false
+        else {return player.readableChats.has(this.chatId)}
+    }
+
+    /**
+    * Whether or not a player can write to this shared chat.
+    * @param {string} playerName - The player name to check.
+    * @returns {boolean} Whether or not the player has write access.
+    */
+    canWrite(playerName) {
+        const player = GameManager.getPlayer(playerName)
+        if (!player) return false
+        else {return player.writeableChats.has(this.chatId)}
+    }
+
+    /**
+    * Revokes a player's read/write access and notifies them.
+    * @param {string} playerName - The player name to revoke access from.
+    */
+    revokeRW(playerName) {
+        const player = GameManager.getPlayer(playerName)
+        this.revokeWrite(playerName)
+
+        player.readableChats.delete(this.chatId)
         IOManager.removePlayerFromRoom(playerName, this.chatId)
         IOManager.emitToPlayer(playerName, 'LOST_CHAT_READ_ACCESS', {chatId: this.chatId})
     }
 
+    /**
+    * Revokes a player's write access and notifies them.
+    * @param {string} playerName - The player name to revoke access from.
+    */
     revokeWrite(playerName) {
         const player = GameManager.getPlayer(playerName)
-        player.removeWriteableChat(this.chatId)
-        this.writers.delete(playerName)
+
+        player.writeableChats.delete(this.chatId)
         IOManager.emitToPlayer(playerName, 'LOST_CHAT_WRITE_ACCESS', {chatId: this.chatId})
     }
 
-    revokeRW(playerName) {
-        this.revokeRead(playerName)
-        this.revokeWrite(playerName)
+    /** Removes write access from all players. */
+    writeLock() {
+        const players = GameManager.allPlayers
+        players.forEach((playerName) => {this.revokeWrite(playerName)})
     }
 
-    getMessages() {
-        return this.messages
-    }
+    /** @returns {Array<{messageType: number, senderName: string, contents: string}} - Array of messages sent here. */
+    get messages() {return this._messages}
 
+    /** @returns {string} - The name of this Shared Chat. */
     get name() {return this._name}
 
-    addMessage(messageType, senderName, contents) {
-        const message = {messageType, senderName, contents, receiver: this.chatId}
-        this.messages = [...this.messages, message]
-        IOManager.emitToChat(this.chatId, 'NEW_MESSAGE', message)
-    }
+    /** @returns {string} - The id of this Shared Chat. */
+    get chatId() {return this._chatId}
 
+    /**
+    * Adds a message to the shared chat and notifies its readers with a payload containing relevant message data.
+    * @param {number} messageType - The message type. Refer to MessageType enum for translation.
+    * @param {string} senderName - The name of the sender. Use '[SERVER]' for non-player messages.
+    * @param {string} contents - The contents of the message.
+    */
+    addMessage(messageType, senderName, contents) {
+        const message = {messageType, senderName, contents}
+        this.messages = [...this.messages, message]
+
+        IOManager.emitToRoom(this.chatId, 'NEW_MESSAGE', {message, receiver: this.chatId})
+    }
 }
 
 module.exports = { SharedChat, MessageType }
