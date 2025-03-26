7496cde8fe1a2dfc36ba7f1a1b5acb94ab1a7e30
Lorraine <lorraine.nix.0@gmail.com>
2025-03-26 17:35:08 -0400

    fixed phase timer

--- client/src/components/PhaseTimer.js
+++ client/src/components/PhaseTimer.js
@@ -1,23 +1,27 @@
-import { useGameStore } from '../store/useGameStore'
+import { useGameStore, PhaseType } from '../store/useGameStore'
 
 export default function PhaseTimer() {
     const phaseType = useGameStore(state => state.gamePhaseType)
     const phaseNumber = useGameStore(state => state.gamePhaseNumber)
     const phaseTimeLeft = useGameStore(state => state.gamePhaseTimeLeft)
 
-    let sec = phaseTimeLeft % 60
-    let min = parseInt(phaseTimeLeft / 60) % 60
-    let hrs = parseInt(phaseTimeLeft / (60*60)) % 60
+    let sec = ('00' + phaseTimeLeft % 60).slice(-2)
+    let min = ('00' + parseInt(phaseTimeLeft / 60) % 60).slice(-2)
+    let hrs = ('00' + parseInt(phaseTimeLeft / (60*60)) % 24).slice(-2)
+    let day = parseInt(phaseTimeLeft / (60*60*24))
 
-    const timerText = hrs <= 0 ? min + ':' + sec + ' min' : hrs + ':' + min + ' hrs'
+
+    const timerText = 
+        (day > 0 ? day + ' days, ' : '') + (hrs <= 0 ? min + ':' + sec + ' min' : hrs + ':' + min + ' hrs')
     const phaseText = 
-        phaseType == phaseType.DAY ? '☼' + phaseNumber :
-        phaseType == phaseType.NIGHT ? '☾' + phaseNumber : 
-        '⧖'
+        phaseType == PhaseType.DAY ? '☼' + phaseNumber :
+        phaseType == PhaseType.NIGHT ? '☾' + phaseNumber : 
+        phaseType == PhaseType.LOBBY ? '⧖' : 
+        '?'
 
     return (
         <label>
-            { phaseText } { timerText }
+            { phaseText } - { timerText }
         </label>
     )
 }
